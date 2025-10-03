a256049d814e95d25dda7083eec52e4f3d5d720f
ramses3studio-a11y <ramses3studio@gmail.com>
2025-10-03 15:14:04 +0200

    update

--- PROGRAMACION_DAW/TEMA1/proyecto-inicial/src/Main.java
+++ PROGRAMACION_DAW/TEMA1/proyecto-inicial/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+// ESTO ES UN COMENTARIO
+// TODO Me falta hacer esto tatatatatat
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en java");
+        System.out.println("Esta es la segunda linea del programa");
+    }
+
+    public void nombreMetodo(){
+
+    }
+
 }
+//todo esto esta pendiente
